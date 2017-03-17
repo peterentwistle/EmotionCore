8dd2cd96333dd430aef529dd00bf9a42dc4d4df2
Peter Entwistle <petertheanimator@gmail.com>
2017-03-17 14:56:41 +0000

    Added a new aggregate target bo build universal framework. Added the framework to the Sources folder

---
+++ build/EmotionCore.build/Debug-iphoneos/EmotionCore.build/DerivedSources/EmotionCore_vers.c
@@ -0,0 +1,5 @@
+ extern const unsigned char EmotionCoreVersionString[];
+ extern const double EmotionCoreVersionNumber;
+
+ const unsigned char EmotionCoreVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:EmotionCore  PROJECT:EmotionCore-1" "\n";
+ const double EmotionCoreVersionNumber __attribute__ ((used)) = (double)1.;
---
+++ build/EmotionCore.build/Debug-iphonesimulator/EmotionCore.build/Objects-normal/x86_64/EmotionCore_vers.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/Peter/Library/Mobile\ Documents/com~apple~CloudDocs/Development/FinalYearProject/EmotionCore/build/EmotionCore.build/Debug-iphonesimulator/EmotionCore.build/DerivedSources/EmotionCore_vers.c
