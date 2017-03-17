d5baba6bd3c461837481bee88579921f451ad576
Peter Entwistle <petertheanimator@gmail.com>
2017-03-17 15:07:21 +0000

    Removed build directory from being tracked

--- build/EmotionCore.build/Debug-iphoneos/EmotionCore.build/Objects-normal/arm64/EmotionCore_vers.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/Peter/Library/Mobile\ Documents/com~apple~CloudDocs/Development/FinalYearProject/EmotionCore/build/EmotionCore.build/Debug-iphoneos/EmotionCore.build/DerivedSources/EmotionCore_vers.c
--- build/EmotionCore.build/Debug-iphonesimulator/EmotionCore.build/Objects-normal/x86_64/EmotionCore_vers.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/Peter/Library/Mobile\ Documents/com~apple~CloudDocs/Development/FinalYearProject/EmotionCore/build/EmotionCore.build/Debug-iphonesimulator/EmotionCore.build/DerivedSources/EmotionCore_vers.c
